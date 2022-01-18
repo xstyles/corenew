09d559658e72f73c73922ddeef777c82c2761cd8
xstyles <ask2invite@gmail.com>
2022-01-19 01:45:18 +0530

    suppress field hints in register form

--- wp-content/themes/twentytwenty/buddypress/members/register.php
+++ wp-content/themes/twentytwenty/buddypress/members/register.php
@@ -58,7 +58,7 @@
 									$field_type = bp_xprofile_create_field_type( bp_get_the_profile_field_type() );
 									$field_type->edit_field_html();
 
-									bp_nouveau_xprofile_edit_visibilty();
+									// bp_nouveau_xprofile_edit_visibilty();
 									?>
 
 									</fieldset>
